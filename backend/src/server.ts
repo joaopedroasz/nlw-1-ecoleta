18c94727e9ebe148432c18b7136e63e9f06ed9b8
João Pedro <johnsj.p@hotmail.com>
2020-06-02 16:31:38 -0300

    create server file

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { resolve } from 'path';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
+
+app.listen(8000);
