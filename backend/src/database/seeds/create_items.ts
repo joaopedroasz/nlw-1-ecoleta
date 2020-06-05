dec58dc538f58bf24437cb1f6efccf1f223de0c9
João Pedro <johnsj.p@hotmail.com>
2020-06-05 18:33:24 -0300

    add new item to seeds

--- backend/src/database/seeds/create_items.ts
+++ backend/src/database/seeds/create_items.ts
@@ -4,7 +4,7 @@ export async function seed(knex: Knex): Promise<void> {
   await knex('items').insert([
     { title: 'Lâmpadas', image: 'lampadas.svg' },
     { title: 'Pilhas e Baterias', image: 'baterias.svg' },
-    { title: 'Papéis e Papelão', image: 'lampadas.svg' },
+    { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
     { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
     { title: 'Óleo de Cozinha', image: 'oleo.svg' },
     { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
